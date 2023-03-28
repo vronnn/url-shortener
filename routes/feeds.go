d1c87210d75b2d39d1c630f032290a4a44bfce58
Rencist <dafianakhdan@gmail.com>
2023-03-28 21:27:45 +0700

    refactor: remove middleware feeds

--- routes/feeds.go
+++ routes/feeds.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"gin-gorm-clean-template/controller"
-	"gin-gorm-clean-template/middleware"
 	"gin-gorm-clean-template/service"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +10,6 @@ import (
 func FeedsRoutes(router *gin.Engine, FeedsController controller.FeedsController, jwtService service.JWTService) {
 	FeedsRoutes := router.Group("/api/feeds")
 	{
-		FeedsRoutes.GET("", middleware.CreateShortUrlAuthenticate(jwtService, false), FeedsController.GetAllFeeds)
+		FeedsRoutes.GET("", FeedsController.GetAllFeeds)
 	}
 }
