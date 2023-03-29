9072b6189ff7594f0c3a93baf02d0786fe1e96c2
Rencist <dafianakhdan@gmail.com>
2023-03-29 23:04:30 +0700

    fix: add form input

--- dto/private.go
+++ dto/private.go
@@ -4,5 +4,5 @@ import "github.com/google/uuid"
 
 type PrivateUpdateDTO struct {
 	ID        	uuid.UUID   `gorm:"primary_key;not_null" json:"id"`
-	Password 	string 		`json:"password"`
+	Password 	string 		`json:"password" form:"password"`
 }
